afeffa44c3464477db9320dce7539ab3737f3f2e
LucasCoppola <lucascoppola21@gmail.com>
2025-03-14 13:33:17 -0300

    fix drop table tests

--- tests/integration/test_drop_table.c
+++ tests/integration/test_drop_table.c
@@ -23,40 +23,36 @@ void tearDown(void) { test_integration_ctx_teardown(&ctx, TEST_DB); }
 void test_drop_table_success(void) {
   const char *create_query = "CREATE TABLE users (id INT, name TEXT);";
   printf("Query: %s\n", create_query);
-  TokenizerState *state = setup_tokenizer(create_query);
-  TEST_ASSERT_NOT_NULL(state);
+  ctx.state = setup_tokenizer(create_query);
+  TEST_ASSERT_NOT_NULL(ctx.state);
 
-  ASTNode *node = parser_table_create(state->tokens, state->token_count);
-  TEST_ASSERT_NOT_NULL(node);
+  ctx.node = parser_table_create(ctx.state->tokens, ctx.state->token_count);
+  TEST_ASSERT_NOT_NULL(ctx.node);
 
-  SemanticResult semantic_result = semantic_analyze(ctx.db, node);
+  SemanticResult semantic_result = semantic_analyze(ctx.db, ctx.node);
   TEST_ASSERT_EQUAL(SEMANTIC_SUCCESS, semantic_result);
 
   Table *table = NULL;
-  TableResult create_result = table_create(ctx.db, node, &table);
+  TableResult create_result = table_create(ctx.db, ctx.node, &table);
   TEST_ASSERT_EQUAL(TABLE_SUCCESS, create_result);
   TEST_ASSERT_NOT_NULL(table);
 
-  if (state) {
-    tokenizer_free(state);
-    state = NULL;
-  }
-  if (node) {
-    ast_free(node);
-    node = NULL;
-  }
+  ast_free(ctx.node);
+  tokenizer_free(ctx.state);
+  ctx.node = NULL;
+  ctx.state = NULL;
 
   const char *drop_query = "DROP TABLE users;";
-  state = setup_tokenizer(drop_query);
-  TEST_ASSERT_NOT_NULL(state);
+  ctx.state = setup_tokenizer(drop_query);
+  TEST_ASSERT_NOT_NULL(ctx.state);
 
-  node = parser_table_drop(state->tokens);
-  TEST_ASSERT_NOT_NULL(node);
+  ctx.node = parser_table_drop(ctx.state->tokens);
+  TEST_ASSERT_NOT_NULL(ctx.node);
 
-  semantic_result = semantic_analyze(ctx.db, node);
+  semantic_result = semantic_analyze(ctx.db, ctx.node);
   TEST_ASSERT_EQUAL(SEMANTIC_SUCCESS, semantic_result);
 
-  ExecuteResult exec_result = execute_drop_table(ctx.db, node);
+  ExecuteResult exec_result = execute_drop_table(ctx.db, ctx.node);
   TEST_ASSERT_EQUAL(EXECUTE_SUCCESS, exec_result);
 
   // Verify the table no longer exists
@@ -76,13 +72,13 @@ void test_drop_table_success(void) {
 void test_drop_table_nonexistent(void) {
   const char *drop_query = "DROP TABLE nonexistent_table;";
   printf("Query: %s\n", drop_query);
-  TokenizerState *state = setup_tokenizer(drop_query);
-  TEST_ASSERT_NOT_NULL(state);
+  ctx.state = setup_tokenizer(drop_query);
+  TEST_ASSERT_NOT_NULL(ctx.state);
 
-  ASTNode *node = parser_table_drop(state->tokens);
-  TEST_ASSERT_NOT_NULL(node);
+  ctx.node = parser_table_drop(ctx.state->tokens);
+  TEST_ASSERT_NOT_NULL(ctx.node);
 
-  SemanticResult semantic_result = semantic_analyze(ctx.db, node);
+  SemanticResult semantic_result = semantic_analyze(ctx.db, ctx.node);
   TEST_ASSERT_EQUAL(SEMANTIC_TABLE_NOT_FOUND, semantic_result);
 }
 
